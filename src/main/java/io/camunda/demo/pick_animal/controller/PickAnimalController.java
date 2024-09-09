3ca347b8874b40a65628ef0cddaf42935599032f
Mark Cheung <fwmarkcheung@gmail.com>
2024-09-09 05:11:12 -0400

    Removed unused imports

--- src/main/java/io/camunda/demo/pick_animal/controller/PickAnimalController.java
+++ src/main/java/io/camunda/demo/pick_animal/controller/PickAnimalController.java
@@ -1,25 +1,17 @@
 package io.camunda.demo.pick_animal.controller;
 
-import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import io.camunda.demo.pick_animal.model.Userchoice;
 import io.camunda.demo.pick_animal.service.PickAnimalService;
-import org.apache.commons.io.IOUtils;
 
 @RestController
 public class PickAnimalController {
-
-  private final static Logger LOG = LoggerFactory.getLogger(PickAnimalController.class);
 
   @Autowired
   private PickAnimalService work;
